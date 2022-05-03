d71f4f57469c0625477e55d3d2c25e4c86dc7e1a
Coderlancce <coderlancce@gmail.com>
2022-05-03 12:32:56 -0500

    9 task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         numbern = number % -10
+        numbern = numbern * -1
     else:
         numbern = number % 10
     print("{}".format(numbern), end="")
