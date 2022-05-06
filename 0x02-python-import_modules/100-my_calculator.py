fbe45f85670fa761caf19451832dc3c74fb7a7c2
Coderlancce <coderlancce@gmail.com>
2022-05-05 20:10:29 -0500

    6 task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit 1
+        exit(1)
 
     a = int(argv[2])
     b = int(argv[4])
@@ -20,4 +20,4 @@ if __name__ == "__main__":
          print("{} / {} = {}".format(argv[2], argv[4], div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit 1
+        exit(1)
