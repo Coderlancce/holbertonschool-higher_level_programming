e4d16a9da87bba0f6945026a140d9621543b5910
Coderlancce <coderlancce@gmail.com>
2022-05-23 12:54:20 -0500

    0 task v 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             counterr += 1
         print("")
         return x
-    except indexError:
+    except safe_print_list_error:
         print("")
         return counterr
