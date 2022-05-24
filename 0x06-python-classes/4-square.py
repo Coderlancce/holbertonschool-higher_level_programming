afed58b76974083e1f4d396d9f3781526d6f70d2
Coderlancce <coderlancce@gmail.com>
2022-05-24 17:49:19 -0500

    4 task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Write a class Square"""
+
+
 class Square:
+    """Represent a square"""
+
     def __init__(self, size=0):
+        """Module __init__ for size square validation"""
         if type(size) is not int:
             raise TypeError("size mustbe an integrer")
         elif size < 0:
@@ -8,14 +14,17 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Method area return the square Area"""
         return(self.__size ** 2)
 
     @property
     def size(self):
+        """Method property of size and return the size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Method size_setter atributte evaluate the value"""
         if type(value) is not int:
             raise TypeError("size must be an integrer")
         elif value < 0:
