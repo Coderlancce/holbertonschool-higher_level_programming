6fcfd93b2254596834d76cc2b4a9926455a1a102
Coderlancce <coderlancce@gmail.com>
2022-05-24 17:37:49 -0500

    3 task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Write a class Square"""
+
+
 class Square:
+    """Represent a square"""
+
     def __init__(self, size=0):
+        """Module __init__ for size square validation"""
         if type(size) is not int:
             raise TypeError("size mustbe an integrer")
         elif size < 0:
@@ -8,4 +14,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Method area return the square Area"""
         return(self.__size ** 2)
