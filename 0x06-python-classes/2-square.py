3b7af03831e55f8c163582516ff33f89912d12c7
Coderlancce <coderlancce@gmail.com>
2022-05-24 17:35:46 -0500

    2 task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Write a class Square"""
+
+
 class Square:
+    """Represent a square"""
+
     def __init__(self, size=0):
+        """Module __init__ for size square validation"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
