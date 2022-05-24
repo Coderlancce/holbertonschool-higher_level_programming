4e48c3be41791d4c558de2990f67b2f3eb0b99b0
Coderlancce <coderlancce@gmail.com>
2022-05-24 17:58:31 -0500

    5 task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""Write a class Square"""
+
+
 class Square:
+    """Represent a square"""
+
     def __init__(self, size=0):
+        """Module __init__ for size square validation"""
         self.size = size
 
     @property
     def size(self):
+        """Method property of size and return the size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """Method size_setter atributte evaluate the value"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -16,10 +24,11 @@ class Square:
         self.__size = value
 
     def area(self):
-        sq_area = self.__size ** 2
-        return (sq_area)
+        """Method area return the square Area"""
+        return self.__size ** 2 
 
     def my_print(self):
+        """Method my_print print a square"""
         if self.__size == 0:
             print("")
         for i in range(self.__size):
