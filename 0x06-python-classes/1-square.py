5263dd0c5fac60ebbbb787ead3034569b85fc0bc
Coderlancce <coderlancce@gmail.com>
2022-05-24 15:57:12 -0500

    1 task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Write a class Square"""
 
 
 class Square:
+    """Write a class Square that defines a square"""
+
     def __init__(self, size):
+        """Module __init__ for size square"""
         self.__size = size
