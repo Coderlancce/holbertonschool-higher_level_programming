1f5e0a12b055f1e32b6db2bfb0b9fed309184c6a
Coderlancce <coderlancce@gmail.com>
2022-06-07 18:11:27 -0500

    1 task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 function that returns the list of available attributes and methods of an object
 """
 
+
 def lookup(obj):
     """
     return the list of available attributes and methods of an object
