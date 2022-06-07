279cf8a2f42141d31f437005d5526935ecd15141
Coderlancce <coderlancce@gmail.com>
2022-06-07 18:30:51 -0500

    6 task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """ incomplepublic class method """
+        raise Exception("area() is not implemented")
