91fa558e1762b2b9e90abada8d74112343f38ae7
Coderlancce <coderlancce@gmail.com>
2022-06-08 17:28:41 -0500

    11 task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" white a class student """
+
+
+class Student:
+    """ Class Student that defines a Student obj """
+
+    def __init__(self, first_name, last_name, age):
+        """ Class Student constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            answer_list = {}
+
+            for count_inp in range(len(attrs)):
+                for count_out in obj:
+                    if attrs[count_inp] == count_out:
+                        answer_list[cunt_out] = obj[count_out]
+
+            return answer_list
+
+        return obj
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for atr in json:
+            self.__dict__[atr] = json[atr]
