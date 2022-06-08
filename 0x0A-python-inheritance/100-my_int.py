735fad1744c72e6b825beae08c6b38b0d11e5c65
Coderlancce <coderlancce@gmail.com>
2022-06-07 20:02:19 -0500

    12 task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class MyInt that inherits from int """
+
+
+class MyInt(int):
+    """ a rebel int inherited class """
+    def __eq__(self, other):
+        """ a == b but reversed """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ a != b but reversed """
+        return super().__eq__(other)
