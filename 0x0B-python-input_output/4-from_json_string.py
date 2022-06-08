003df78915dc2d11a2683a8bfaeb38e38d3f7a6a
Coderlancce <coderlancce@gmail.com>
2022-06-08 15:36:24 -0500

    4 task

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" from JSON representation """
+
+
+def from_json_string(my_str):
+    """ returns an object (Python data structure)
+    represented by a JSON string """
+    return json.loads(my_str)
