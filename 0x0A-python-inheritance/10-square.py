01e21402caa3b7dd9a91aeef0ac46661028d612b
Coderlancce <coderlancce@gmail.com>
2022-06-07 19:01:50 -0500

    10 task

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" import class to inherit for """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class inherited from Reactangle """
+
+    def __init__(self, size):
+        """ inizilalice it """
+        super().integer_validator("size", size)
+        self._Square__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ inherit get area function """
+        return super().area()
