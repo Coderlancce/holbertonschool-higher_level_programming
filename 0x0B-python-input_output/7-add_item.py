d89adf2a74f93a6242c1408923f1c3b9676b4855
Coderlancce <coderlancce@gmail.com>
2022-06-08 20:36:04 -0500

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,12 +10,15 @@ from sys import argv as av
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    fileAdd = "add_item.json"
-    buff = []
+buff = []
 
-    if path.isfile(fileAdd):
-        buff = load_from_json_file(fileAdd)
+if path.isfile("add_item.json") is False:
+    save_to_json_file(buff, "add_item.json")
 
-    buff.extend(av[1:])
-    save_to_json_file(buff, fileAdd)
+buff = load_from_json_file("add_item.json")
+
+if sys.av[1:]:
+    for i in sys.av[1:]:
+        buff.append(i)
+
+save_to_json_file(buff, "add_item.json")
