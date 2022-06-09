7222a32f190062f27398457b4be405e149eea5a1
Coderlancce <coderlancce@gmail.com>
2022-06-08 20:32:24 -0500

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,13 +3,12 @@
 script that adds all arguments to a Python list,
 and then save them to a file
 """
-from os import path
-from sys
 
+from os import path
+from sys import argv as av
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 if __name__ == "__main__":
     fileAdd = "add_item.json"
