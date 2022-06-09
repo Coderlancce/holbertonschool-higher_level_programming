84b281ed3ee3c7d8c190891ccda49a2fd1d87a19
Coderlancce <coderlancce@gmail.com>
2022-06-08 20:13:58 -0500

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ script that adds all arguments to a Python list,
 and then save them to a file """
-
-
 from os import path
 from sys import argv as av
+
 
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
