4e8391e67cc36de5fd83478fad9db2a2d2f041ef
Coderlancce <coderlancce@gmail.com>
2022-06-08 20:39:55 -0500

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""
-script that adds all arguments to a Python list,
-and then save them to a file
+"""This script that adds all arguments
+to a Python list, and then save them to a file
 """
 
 
+import sys
 from os import path
-from sys
-
+"""Import the json module and sys module"""
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""Import the save to json file function"""
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Import the save to json file function"""
 
-buff = []
+ret_list = []
 
 if path.isfile("add_item.json") is False:
-    save_to_json_file(buff, "add_item.json")
+    save_to_json_file(ret_list, "add_item.json")
 
-buff = load_from_json_file("add_item.json")
+ret_list = load_from_json_file("add_item.json")
 
 if sys.argv[1:]:
     for i in sys.argv[1:]:
-        buff.append(i)
+        ret_list.append(i)
 
-save_to_json_file(buff, "add_item.json")
+save_to_json_file(ret_list, "add_item.json")
