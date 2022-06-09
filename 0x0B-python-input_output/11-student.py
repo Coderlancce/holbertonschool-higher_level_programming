d40f6790cbc47593f530c8d5abeb0fef07cc5985
Coderlancce <coderlancce@gmail.com>
2022-06-08 21:10:16 -0500

    11 task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,14 +4,13 @@
 
 class Student:
     """ Class Student that defines a Student obj """
-
     def __init__(self, first_name, last_name, age):
         """ Class Student constructor """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ Method that returns directory description """
         answer_list = {}
         if type(attrs) is list:
@@ -21,7 +20,7 @@ class Student:
                         answer_list[item] = getattr(self, item)
 
                 return answer_list
-            
+
         return self.__dict__
 
     def reload_from_json(self, json):
