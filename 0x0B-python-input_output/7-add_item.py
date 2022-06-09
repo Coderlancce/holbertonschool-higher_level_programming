975f563622b2f9bdec5f328c3b854d6c3d955b2c
Coderlancce <coderlancce@gmail.com>
2022-06-08 20:27:49 -0500

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,16 @@ and then save them to a file
 """
 
 from os import path
-from sys import argv as av
+from sys
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+    my_list = load_from_json_file("add_item.json")
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
-save_file(my_list, "add_item.json")
+save_to_json_file(my_list, "add_item.json")
