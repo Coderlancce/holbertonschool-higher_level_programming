c17af31b514b56da6886b568e454fec2e20c2cde
Coderlancce <coderlancce@gmail.com>
2022-06-08 20:38:36 -0500

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ and then save them to a file
 
 
 from os import path
-from sys import argv as av
+from sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -18,8 +18,8 @@ if path.isfile("add_item.json") is False:
 
 buff = load_from_json_file("add_item.json")
 
-if sys.av[1:]:
-    for i in sys.av[1:]:
+if sys.argv[1:]:
+    for i in sys.argv[1:]:
         buff.append(i)
 
 save_to_json_file(buff, "add_item.json")
