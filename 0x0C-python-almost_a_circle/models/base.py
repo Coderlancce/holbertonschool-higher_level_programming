5b497d71c02f7ff8aacf796d5d0deeeaef757469
Coderlancce <coderlancce@gmail.com>
2022-06-14 16:22:27 -0500

    [fix]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ create a Base class """
 import json
-
+import csv
+import os.path
 
 class Base:
     """ Base class """
@@ -18,3 +19,22 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ representation of list dictionaries """
+        return json.dumps(list_dictionaries) if list_dictionaries else "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Save object in a file """
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
+
+
+        if not list_objs:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
