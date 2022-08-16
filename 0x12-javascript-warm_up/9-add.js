f3e7dd0c97bbeca5cd4d271a9429dee73a13d680
Coderlancce <coderlancce@gmail.com>
2022-08-16 07:52:27 -0500

    [OK] function sum

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+const firstNumber = parseInt(argv[2]);
+const secondNumber = parseInt(argv[3]);
+
+function add(a, b) {
+  return (a + b);
+}
+
+console.log(add(firstNumber, secondNumber));
