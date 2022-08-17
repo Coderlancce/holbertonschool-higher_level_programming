b43052ad2777d6682b84740a9bb8b6a86b08a1a3
Coderlancce <coderlancce@gmail.com>
2022-08-16 20:07:03 -0500

    [OK] semistrand pass

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,23 +2,23 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-    constructor (size) {
-	super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint (c) {
-	let charToPrint = c;
-	let width = '';
-	let i = 0;
-	if (c === undefined) {
-	    charToPrint = 'X';
-	}
-	while (i < this.width) {
-	    width += charToPrint;
-	    i++;
-	}
-	for (i = 0; i < this.height; i++) {
-	    console.log(width);
-	}
+  charPrint (c) {
+    let charToPrint = c;
+    let width = '';
+    let i = 0;
+    if (c === undefined) {
+      charToPrint = 'X';
     }
+    while (i < this.width) {
+      width += charToPrint;
+      i++;
+    }
+    for (i = 0; i < this.height; i++) {
+      console.log(width);
+    }
+  }
 };
