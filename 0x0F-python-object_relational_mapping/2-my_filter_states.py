4c937d67c5a1bbcbac0b6812332f11f2a003cc35
Coderlancce <coderlancce@gmail.com>
2022-08-30 12:44:32 -0500

    [OK] task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     query = """
-    SELECT * FROM states WHERE name='{:s}'
+    SELECT * FROM states WHERE BINARY name='{:s}'
     ORDER BY states.id ASC;
     """.format(state_name)
 
